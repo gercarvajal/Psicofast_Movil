7154ed511d45983ee7e69f896442d73dc57bded3
gercarvajal <nikocarvajal1996@gmail.com>
2022-06-05 14:59:49 -0400

    cambios

--- app/src/main/java/com/gerca/myapplication/MainActivity.java
+++ app/src/main/java/com/gerca/myapplication/MainActivity.java
@@ -7,20 +7,37 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
     Button ingre;
+    TextView recupe;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         ingre=(Button)findViewById(R.id.btningresar);
+        recupe=(TextView)findViewById(R.id.recuperar) ;
 
         ingre.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
+                //Intent i= new Intent( MainActivity.this,MenuPrincipal.class);
+               // startActivity(i);
+                Toast.makeText (getApplicationContext(),
+                                "Pulsaste el boton Ingresar",Toast.LENGTH_LONG)
+                        .show();
+            }
+        });
+
+        recupe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
                 Intent i= new Intent( MainActivity.this,MenuPrincipal.class);
                 startActivity(i);
 
